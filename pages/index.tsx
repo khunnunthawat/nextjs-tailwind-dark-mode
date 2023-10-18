56b72a6d0a3cd95ca8af3d8a185682e4b3bdba21
khunnunthawat <khunnunthawat@gmail.com>
2023-10-18 13:00:07 +0700

    Added a simple title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-lg font-semibold text-blue-500 hover:text-pink-500">
-      Nextjs Tailwind <span className="text-gray-900 dark:text-white">Dark Mode</span>
+      Tailwind <span className="text-gray-900 dark:text-white">Dark Mode</span>
     </div>
   )
 }
